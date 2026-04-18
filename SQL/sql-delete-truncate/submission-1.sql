575f3c4f604645a750293545869a0bd4774ed69f
Samir Alizada <143426030+mimir45@users.noreply.github.com>
2026-04-18 18:48:49 +0400

    Add: sql-delete-truncate - submission-1

---
+++ SQL/sql-delete-truncate/submission-1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+INSERT INTO students (id, name)
+  VALUES (1, 'Alice'),
+         (2, 'Bob'),
+         (3, 'Charlie');
+-- Do not modify above this line. --
+
+
+Truncate table students;
+
+-- Do not modify below this line. --
+SELECT * FROM students;
